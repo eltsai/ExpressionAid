4a09e6b65c01d1c3e6921f56588fa6f3b9aa857a
kychern <kychern@outlook.com>
2018-06-03 01:06:15 +0800

    add fill_matrix.py

---
+++ testdlib.py
@@ -0,0 +1,17 @@
+import sys
+import dlib
+from skimage import io
+
+detector = dlib.get_frontal_face_detector() 
+window = dlib.image_window()  
+img = io.imread("test.jpg")
+
+dets = detector(img, 1)  
+print("Number of faces detected: {}".format(len(dets))) 
+for i, d in enumerate(dets):
+    print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(i, d.left(), d.top(), d.right(), d.bottom()))
+
+window.clear_overlay()
+window.set_image(img) 
+window.add_overlay(dets)
+dlib.hit_enter_to_continue() 
